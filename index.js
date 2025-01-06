6a5d26e10f9b04e07d14e2d39c1115e9fc3d8482
mstsurnalyakter <surnalyms@gmail.com>
2025-01-05 19:17:09 -0700

    update MongoDB connection string configuration for production and local environments

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 
-// const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.50udlth.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.50udlth.mongodb.net/?retryWrites=true&w=majority`;
 
-const uri = "mongodb://localhost:27017";
+// const uri = "mongodb://localhost:27017";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
